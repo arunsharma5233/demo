39e334523b471c707040134c3321e1afc185d7e7
arunsharma5233 <arunsharma802264@gmail.com>
2023-04-24 17:06:45 +0530

    fourth

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('posts/{post:slug}', function(Post $post) {
 });
 Route::get('/categories/{category:slug}', function(Category $category){
     return view('posts',[
-        'posts' =>$category->posts,//->load(['category','author'])
+        'posts' =>$category->posts,
         'currentCategory'=>$category,
         'categories' =>Category::all()
     ]);
